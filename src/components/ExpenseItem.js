9adaeeb3e4c77a86f75b8c7d8897637175d62094
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-05-05 14:47:11 +0530

    added three expense items in the componenet tree

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <li>Food Rs.10</li>
+      <li>Petrol Rs.100</li>
+      <li>Movies Rs 200</li>
+    </div>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,11 @@
+import ExpenseItem from './ExpenseItem.js'
+
+function ExpenseItems(){
+    return <div>
+        <h1> Expense Item's!</h1>
+        <ExpenseItem></ExpenseItem>
+        
+    </div>
+}
+
+export default ExpenseItems
