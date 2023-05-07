5fca99eab50a16b562b1ca5a485a896f3259e68f
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-05-07 14:51:22 +0530

    delete functionality added after learning event listner in react

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,28 @@
-import ExpenseDate from './ExpenseDate.js'
+import ExpenseDate from "./ExpenseDate.js";
 import "./ExpenseItem.css";
-import ExpenseDetails from  './ExpenseDetails.js';
-import Card from '../UI/Card'
+import ExpenseDetails from "./ExpenseDetails.js";
+import Card from "../UI/Card";
 
-let ExpenseItem=(obj) =>{
+let ExpenseItem = (obj) => {
+  let deleteExpense = (e) => {
+    console.log("hey clicked");
+
+    let currentCard = e.target.closest(".expense-item");
+    let parentOfExpense = currentCard.parentElement;
+    parentOfExpense.removeChild(currentCard);
+  };
   
   return (
     <Card className="expense-item">
       <ExpenseDate date={obj.date} />
-      <ExpenseDetails title={obj.title} location={obj.location} amount={obj.amount}></ExpenseDetails>
+      <ExpenseDetails
+        title={obj.title}
+        location={obj.location}
+        amount={obj.amount}
+      ></ExpenseDetails>
+      <button onClick={deleteExpense}>Delete Expense</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
